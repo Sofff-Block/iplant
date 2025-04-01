9185d106434a09719d0fdbe8ef4befc8deed3e4f
Daniel Greßmann <daniel@gressmann.online>
2025-04-01 14:35:28 +0200

    successfully delet consol.log + add image size prop

--- components/PlantCard.js
+++ components/PlantCard.js
@@ -4,41 +4,36 @@ import styled from "styled-components";
 export default function PlantCard({ image, name, botanicalName }) {
   return (
     <PlantCardWrapper>
-        <PlantImageWrapper>
-      <StyledImage alt={name} src={image} fill="true"></StyledImage></PlantImageWrapper>
+      <PlantImageWrapper>
+        <StyledImage
+          sizes="(max-width: 150px)"
+          alt={name}
+          src={image}
+          fill="true"
+        ></StyledImage>
+      </PlantImageWrapper>
       <h2> {name}</h2>
       <p>{botanicalName}</p>
-     
     </PlantCardWrapper>
   );
 }
 
+const PlantImageWrapper = styled.div`
+  width: 150px;
+  height: 200px;
+  position: relative;
+  border: 2px solid black;
+`;
 
-const PlantImageWrapper = styled.div `
+const PlantCardWrapper = styled.div`
+  display: flex;
+  border: 2px solid purple;
+  width: 150px;
+  text-align: center;
 
-width:150px;
-height:200px;
-position:relative;
-border: 2px solid black;
-`
-
-const  PlantCardWrapper = styled.div`
-display: flex;
-border: 2px solid purple;
-width:150px;
-text-align: center;
-
-flex-direction: column;
-
- 
-`
-
-
+  flex-direction: column;
+`;
 
 const StyledImage = styled(Image)`
-
-
-object-fit:cover;
-
-
-`
+  object-fit: cover;
+`;
