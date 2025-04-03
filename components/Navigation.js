125d262b0b1e3b95f81aab3dd3e4cd29be0e7702
Sofia Block <fleurfromtheblock@gmail.com>
2025-04-03 11:08:07 +0200

    styled navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -5,13 +5,23 @@ import House from "@/public/house.svg";
 
 export default function Navigation() {
   return (
-    <nav>
+    <StyledNav>
       <Link href="/">
         <House />
       </Link>
       <Link href="/">
         <Flower />
       </Link>
-    </nav>
+    </StyledNav>
   );
 }
+
+const StyledNav = styled.nav`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-evenly;
+  background-color: hotpink;
+`;
