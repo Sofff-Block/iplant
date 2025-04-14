dcd35eb6e58468addc29ad653f1b6b44f83c4958
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-04-14 15:24:59 +0200

    connect database

---
+++ db/models/PlantsSchema.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const plantsSchema = new Schema({
+  name: { type: String, required: true },
+  botanicalName: { type: String, required: true },
+  imageUrl: { type: String },
+  waterNeed: { type: String, required: true },
+  lightNeed: { type: String, required: true },
+  fertiliserSeason: [String],
+  description: { type: String },
+});
+
+const Plant = mongoose.models.Plant || mongoose.model("Plant", plantsSchema);
+
+export default Plant;
---
+++ pages/api/plants/index.js
@@ -0,0 +1,14 @@
+import dbConnect from "@/db/connect";
+import Plant from "@/db/models/PlantsSchema";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const plants = await Plant.find();
+    response.status(200).json(plants);
+    return;
+  } else {
+    response.status(405).json({ status: "Method not allowed." });
+  }
+}
