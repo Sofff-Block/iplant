512ee2ced896c84d56714cdcf3ef15429f0a5dd3
Lisa Hauschild <lisa_hauschild@hotmail.com>
2025-04-15 16:16:34 +0200

    add default picture

--- db/models/PlantsSchema.js
+++ db/models/PlantsSchema.js
@@ -5,7 +5,11 @@ const { Schema } = mongoose;
 const plantsSchema = new Schema({
   name: { type: String, required: true },
   botanicalName: { type: String, required: true },
-  imageUrl: { type: String },
+  imageUrl: {
+    type: String,
+    default:
+      "https://images.pexels.com/photos/2587313/pexels-photo-2587313.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  },
   waterNeed: { type: String, required: true },
   lightNeed: { type: String, required: true },
   fertiliserSeason: { type: [String], default: [] },
